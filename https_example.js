a3ed03b890e43da8169d89aca4ce2367e3455e7a
Nick Lee <nickoless.lee@gmail.com>
2018-01-21 01:03:32 +0000

    modified file to return HTML

--- https_example.js
+++ https_example.js
@@ -9,8 +9,15 @@ var options = {
 
 // this will be called by https when the request is made.
 
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk){
+    console.log('[-- CHUNK OF LENGTH' + chunk.length + '--]');
+    console.log(chunk.toString());
+  });
+
+  console.log('Response: ', response);
 };
 
 console.log("I'm about to make the request!");
